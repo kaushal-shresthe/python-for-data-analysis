bac185db83043ca77f11ae1d484c6285d0bf8f04
kaushal-shresthe <kaushalshrestha441@gmail.com>
2025-07-22 23:06:21 +0545

    Different types of operators in python

---
+++ operators/OperatorPrecedence.py
@@ -0,0 +1,13 @@
+result = 10 + 5 * 2    # 5 * 2 = 10 → 10 + 10 = 20
+print("Result:", result)
+
+# Using parentheses to change precedence
+result = (10 + 5) * 2  # 15 * 2 = 30
+print("With parentheses:", result)
+
+# Complex example
+a = 10
+b = 5
+c = 2
+
+print(a + b * c ** 2)  # c**2 = 4 → b*4 = 20 → a+20 = 30
