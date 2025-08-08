f6214f9c3c1f2d052dda67f24486b9ec42836eca
kaushal-shresthe <kaushalshrestha441@gmail.com>
2025-08-08 16:33:17 +0545

    Implemented 6 patterns using nested loops: Right-Angled Triangle, Number Triangle, Reverse Triangle, Square Numbers, Pyramid of Stars, and Butterfly Pattern

---
+++ LoopStatements/PatternsProblems.py
@@ -0,0 +1,141 @@
+# Pattern 1: Right-Angled Triangle
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+for i in range(1, 6):
+    for j in range(1, i+1):
+        print("*", end=" ")
+    print()
+
+# Pattern 2: Number Triangle
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
+
+# for i in range(1, 6):
+#     for j in range(1, i+1):
+#         print(j, end=" ")
+#     print()
+
+# Pattern 3: Reverse Triangle
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+
+for i in range(1, 6):
+    for j in range(6 - i):
+        print("*", end=" ")
+    print()
+
+
+# Pattern 4: Square Pattern with Same Numbers
+# 1 1 1 1
+# 2 2 2 2
+# 3 3 3 3
+# 4 4 4 4
+
+for i in range(1, 5):
+    for j in range(4):
+        print(i, end=" ")
+    print()
+
+
+# Pattern 5: Pyramid of Stars
+#         *
+#       * * *
+#     * * * * *
+#   * * * * * * *
+# * * * * * * * * *
+
+for i in range(1, 6):
+    for j in range(1, 6 - i):
+        print(" ", end=" ")
+    for k in range(2 * i - 1):
+        print("*", end=" ")
+    print()
+
+
+# Pattern 6: Butterfly Pattern
+# *               *
+# * *           * *
+# * * *       * * *
+# * * * *   * * * *
+# * * * * * * * * *
+# * * * *   * * * *
+# * * *       * * *
+# * *           * *
+# *               *
+
+# Method 1
+loop = 1
+for i in range(1, 10):
+    if i <= 4:
+        for j in range(1, i + 1):
+            print("*", end=" ")
+
+        for k in range(1, 10-2*i):
+            print(" ", end=" ")
+
+        for j in range(1, i + 1):
+            print("*", end=" ")
+
+    if i == 5:
+        for j in range(1, 10):
+            print("*", end=" ")
+
+    if i > 5:
+        for j in range(0, i - 2 * loop):
+            print("*", end=" ")
+
+        for k in range(0, 2 * loop - 1):
+            print(" ", end=" ")
+
+        for j in range(0, i - 2 * loop):
+            print("*", end=" ")
+        loop = loop + 1
+    print()
+
+# Method 2
+n = 5  # Total rows in half (top or bottom)
+# Top half (1 to 5)
+for i in range(1, n + 1):
+    # Left wing
+    for j in range(1, i + 1):
+        print("*", end=" ")
+
+    for k in range(1, 2 * (n - i) + 1):
+        print(" ", end=" ")
+
+    for j in range(1, i + 1):
+        print("*", end=" ")
+
+    print()
+
+# Bottom half (4 to 1)
+for i in range(n - 1, 0, -1):
+
+    for j in range(1, i + 1):
+        print("*", end=" ")
+
+    for k in range(1, 2 * (n - i) + 1):
+        print(" ", end=" ")
+
+    for j in range(1, i + 1):
+        print("*", end=" ")
+
+    print()
+
+
+
+
+
+
+
+
