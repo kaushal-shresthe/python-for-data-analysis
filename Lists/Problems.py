1da2e37695362e0abe710bc7d1be04ff3fa1f935
kaushal-shresthe <kaushalshrestha441@gmail.com>
2025-08-20 22:05:20 +0545

    Add the list problems

---
+++ Lists/Problems.py
@@ -0,0 +1,28 @@
+A = ["Ross", "Rachel", "Monica", "Joe"]
+
+# Write a Program to swap first and fourth element
+print(f"Before Swap: {A}")
+A[0], A[3] = A[3], A[0]
+print(f"After Swap: {A}")
+
+# Write a Program to add a new value at second position
+A.insert(1, "Phoebe")
+print(A)
+
+# Write a program to delete a value from 3rd position
+print(A.pop(2))
+print(A)
+
+B = [13, 7, 12, 10]
+
+# Write a program to mutliply all the element in the list
+mul = 1
+for i in B:
+    mul = mul * i
+print(mul)
+
+# Write a program to get the largest and smallest number from the list
+B.sort()
+print(f"Largest value is {B[-1]}")
+print(f"Smallest value is {B[0]}")
+
